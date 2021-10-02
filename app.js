30f084249f8385631485bce3c427511baa24d463
向山　翔 <hms0175km@gmail.com>
2021-10-02 21:31:52 +0900

    [Finish2-14 ]

---
+++ app.js
@@ -0,0 +1,10 @@
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('オレンジの値段がりんごより安い');
+} else if(orange == apple){
+  alert('オレンジとりんごが同じ値段');
+} else{
+  alert('オレンジの値段がりんごより高い');
+}
